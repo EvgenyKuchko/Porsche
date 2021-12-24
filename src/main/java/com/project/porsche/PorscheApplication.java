f18750a85068989e44da9d22b482b38700af3a25
EvgenyKuchko <kuchkozhenia@gmail.com>
2021-12-24 11:04:37 +0300

    Fix codeStyle

--- src/main/java/com/project/porsche/PorscheApplication.java
+++ src/main/java/com/project/porsche/PorscheApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PorscheApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PorscheApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PorscheApplication.class, args);
+    }
 }
